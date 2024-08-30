b1da787e8e2243eefee212a50cb456cc36ff82c7
Godson okoye <161264678+Godson330@users.noreply.github.com>
2024-08-30 07:03:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import NamePasswordForm from './NamePasswordForm'; 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to My React App</h1>
-      <NamePasswordForm /> {/* This is where the form component is rendered */}
-    </div>
-  );
-}
-
-export default App;
